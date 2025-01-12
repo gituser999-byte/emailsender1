e402d93ff31b8b95e304e36e9bb26f7c74cb5d2a
allsubscriptions101010@gmail.com <allsubscriptions101010@gmail.com>
2025-01-12 03:52:22 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,6 +10,10 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.
 
-  def submit_button_click(self, **event_args):
-    # Display a popup that says 'You clicked the button'
-    alert("You clicked the button")
+def submit_button_click(self, **event_args):
+  # Set 'name' to the text in the 'name_box'
+  name = self.name_box.text
+  # Set 'email' to the text in the 'email_box'
+  email = self.email_box.text
+  # Set 'feedback' to the text in the 'feedback_box'
+  feedback = self.feedback_box.text
